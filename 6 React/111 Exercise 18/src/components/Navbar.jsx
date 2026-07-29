f0642556dab4447175dff705a761ae5590385fa3
Arman Bin Alauddin <160709760+armanbin007@users.noreply.github.com>
2026-07-29 19:56:07 +0600

    Add files via upload

---
+++ 6 React/111 Exercise 18/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar =()=>{
+    return(
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact Us</li>
+            </ul>
+        </nav>
+    )
+}
+export default Navbar
